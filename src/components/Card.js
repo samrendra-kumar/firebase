a75377a517712af4ce7830ea0d84531782092629
Samrendra <samrendrakumar893@gmail.com>
2024-01-10 23:12:42 +0530

    set documents with images updated the UI

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,24 @@
-function Card({ path, title}) {
+
+import { useMemo } from "react";
+
+function Card({ path, title, createdAt }) {
+    const timestamp = useMemo(() => {
+        const date = new Date(createdAt.seconds*1000)
+        return `${new Date(createdAt.seconds*1000)}`
+    }, [])
     return(
         <div className="col mb-5">
             <div className="card" style={{width: "18rem"}}>
-                <img src={path} className="card-img-top" alt={title} />
+                <div style={{
+                    height: "220px", 
+                    backgroundImage: `url(${path})`, 
+                    backgroundSize: "cover", 
+                    backgroundRepeat: "no-repeat"
+                }}>
+
+                </div>
+                <h5 className="text-center">{title}</h5>
+                <p>{timestamp}</p>
             </div>
         </div>
     )
