beed5db319afced8acb1a1464d152389e48b2d8d
Samrendra <samrendrakumar893@gmail.com>
2024-01-11 16:41:48 +0530

    Authenticate and Manage Users

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
 import {getStorage,ref} from "firebase/storage"
 const firebaseConfig = {
@@ -21,4 +18,5 @@ const app =  initializeApp(firebaseConfig) ;
 
  export const db= getFirestore(app) ;
  export const storage=getStorage(app);
+ export const auth= getAuth(app);
 export default app 
